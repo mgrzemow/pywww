4af10aec269f9a9e15f0e45c66dc1fdc1cd447e5
Michal Grzemowski <michal@grzemow.ski>
2023-05-18 22:51:50 +0200

    requitements

--- pywww/settings.py
+++ pywww/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
